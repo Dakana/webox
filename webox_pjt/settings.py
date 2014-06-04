b1cf74e0edb3b34d91f10653eac72c23f66c9ee4
Narciso Avalos <narciso@masfusions-iMac-5.local>
2014-06-04 16:43:12 -0500

    Creating Spotify Models and Files

--- webox_pjt/settings.py
+++ webox_pjt/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'app',
+    'tracks',
+    'artist',
+    'albums',
 )
 
 MIDDLEWARE_CLASSES = (
